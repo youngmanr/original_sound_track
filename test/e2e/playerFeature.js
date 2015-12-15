0962d82bda2c7e94038d5c4ebb269ded27b24e8d
Robert Youngman <rob_youngman@yahoo.com>
2015-12-15 18:44:43 +0000

    play, pause button tests

--- test/e2e/playerFeature.js
+++ test/e2e/playerFeature.js
@@ -1,11 +1,39 @@
 describe('OST Player', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
-    expect(browser.getTitle()).toEqual('Original Sound Track');
+
+  describe('home page', function() {
+    it('has a title', function() {
+      browser.get('http://localhost:8080');
+      expect(browser.getTitle()).toEqual('Original Sound Track');
+    });
   });
 
-  it('has a play button', function() {
-    browser.get('http://localhost:8080');
-    expect(element(by.css('.jp-play')).isPresent()).toBe(true);
+
+  describe('is not playing a track', function() {
+    beforeEach(function() {
+      browser.get('http://localhost:8080');
+    })
+
+    it('should have a play button', function() {
+      expect(element(by.css('.jp-play')).isDisplayed()).toBeTruthy();
+    });
+
+    it('should not have a pause button', function() {
+      expect(element(by.css('.jp-pause')).isDisplayed()).toBeFalsy();
+    });
+  });
+
+  describe('is playing a track', function() {
+    beforeEach(function() {
+      browser.get('http://localhost:8080');
+      element(by.css('.jp-play')).click();
+    })
+
+    it('should not have a play button', function() {
+      expect(element(by.css('.jp-play')).isDisplayed()).toBeFalsy();
+    });
+
+    it('should have a pause button', function() {
+      expect(element(by.css('.jp-pause')).isDisplayed()).toBeTruthy();
+    });
   });
 });
