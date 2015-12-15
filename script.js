2adc9d42a3475a5f1eed44073bc4cb3df3be242e
blancopado <pablo.dominguezblanco@gmail.com>
2015-12-15 16:32:24 +0100

    coordinates to string

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-    $.getJSON("http://developer.echonest.com/api/v4/artist/search?api_key=BG6IJZJJYOKNETBX8&format=json&artist_location=manchester+england&min_familiarity=0.5&sort=hotttnesss-desc&results=50&bucket=artist_location",
-        function(data){
-        // data is JSON response object
-        console.log(data.response);
-        $("#results").append("<li>"+data.response.artists[0].name+"</li>");
-    });
+  $.getJSON("http://developer.echonest.com/api/v4/artist/search?api_key=BG6IJZJJYOKNETBX8&format=json&artist_location=manchester+england&min_familiarity=0.5&sort=hotttnesss-desc&results=50&bucket=artist_location",
+      function(data){
+      // data is JSON response object
+      console.log(data.response);
+      $("#results").append("<li>"+data.response.artists[0].name+"</li>");
+  });
 
   var geolocUrl;
 
@@ -16,16 +16,15 @@ $(document).ready(function() {
 
   function getLocation() {
     console.log('get position');
-    navigator.geolocation.watchPosition(showPosition);
+    navigator.geolocation.watchPosition(showPosition, console.log("failed to get position"), {timeout: 30000});
   };
 
   function showPosition(position) {
-    console.log('show position');
     geolocUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude+ "," + position.coords.longitude
-                    '&key=AIzaSyAGWnWE0GeEPpCYmiy2mXZ9RnDGf_n3JQA';
-
+                     '&key=AIzaSyAGWnWE0GeEPpCYmiy2mXZ9RnDGf_n3JQA';               	
     $.get(geolocUrl, function(response) {
-      console.log(response.results[6].address_components[0].long_name);
+      console.log(response.results[response.results.length-3].address_components[0].long_name);
+    	console.log(response.results);
     });
   };
 });
