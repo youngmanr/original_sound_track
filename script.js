e2bfe0fe27f98fc0fc8a264f738837e19ddaab5f
Andrew Dowell <adowellphoto@gmail.com>
2015-12-15 14:08:11 +0000

    Geolocation strings added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-    $.getJSON("http://developer.echonest.com/api/v4/artist/search?api_key=&format=json&artist_location=manchester+england&min_familiarity=0.5&sort=hotttnesss-desc&results=50&bucket=artist_location",
+    $.getJSON("http://developer.echonest.com/api/v4/artist/search?api_key=BG6IJZJJYOKNETBX8&format=json&artist_location=manchester+england&min_familiarity=0.5&sort=hotttnesss-desc&results=50&bucket=artist_location",
         function(data){
         // data is JSON response object
         console.log(data.response);
         $("#results").append("<li>"+data.response.artists[0].name+"</li>");
     });
+
+  var geolocUrl;
 
   $('button').click(function() {
     console.log('button clicked');
@@ -19,8 +21,12 @@ $(document).ready(function() {
 
   function showPosition(position) {
     console.log('show position');
-  $('#demo').append("Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude);
+    geolocUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude+ "," + position.coords.longitude
+                    '&key=AIzaSyAGWnWE0GeEPpCYmiy2mXZ9RnDGf_n3JQA';
+
+    $.get(geolocUrl, function(response) {
+      console.log(response.results[6].address_components[0].long_name);
+    });
   };
 });
 
