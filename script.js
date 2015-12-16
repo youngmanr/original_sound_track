f014ffd0c6f819acc990f5f5e198812b0ded035f
Andrew Dowell <adowellphoto@gmail.com>
2015-12-16 14:21:12 +0000

    minor changes to functionality

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 
-  var latitude;
-  var longitude;
   var cityName;
   var country;
+  var countryCode;
   var playlist = [];
 
   $('button').click(function() {
@@ -15,8 +14,8 @@ $(document).ready(function() {
   };
 
   function showPosition(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
     var geolocUrl = 'https://maps.googleapis.com/maps/api/geocode/json?&language=en&latlng=' + latitude + "," + longitude
                      '&key=AIzaSyAGWnWE0GeEPpCYmiy2mXZ9RnDGf_n3JQA';
 
@@ -38,74 +37,62 @@ $(document).ready(function() {
         for (var result = 0; result < results.length; result++) {
           for (var component = 0; component < results[result].address_components.length; component++) {
             if(results[result].address_components[component].types.includes('locality')) {
-              cityName = results[result].address_components[component].long_name;
+              cityName = convToParam(results[result].address_components[component].long_name);
             };
           };
         };
-        country = results[results.length-1].address_components[0].long_name;
+        country = convToParam(results[results.length-1].address_components[0].long_name);
       };
 
-      console.log("cityName = " + cityName, "country = " + country);
+      countryCode = results[results.length-1].address_components[0].short_name;
+      console.log("cityName = " + cityName, "country = " + country, "countryCode = " + countryCode);
       console.log(response);
       getArtists();
     });
   };
 
-  // ALTERNATIVE GEOLOCATION - a lot easier
-  // $.get("http://ipinfo.io", function(response) {
-  //   console.log(response.city, response.country, response.region);
-  // }, "jsonp");
-
+  // MAKES ECHONEST API CALL BASED ON cityName AND country
   function getArtists() {
     var echonestUrl = 'http://developer.echonest.com/api/v4/artist/search?api_key=BG6IJZJJYOKNETBX8' +
                   '&format=json' +
-                  // '&artist_location=' + convToParam(cityName) + "+" + convToParam(country) +
                   '&artist_location=' + cityName + '+' + country +
-                  '&min_familiarity=0.1' +
+                  '&min_familiarity=0.5' +
                   '&sort=familiarity-desc&results=35' +
                   '&bucket=artist_location' +
                   '&bucket=songs' + '&bucket=id:spotify';
 
     $.getJSON(echonestUrl,
         function(data){
-        // data is JSON response object
-        var spotifyID;
-        var topTracks;
-        var randomTrack;
 
         console.log(data.response);
         artist = data.response.artists[Math.floor(Math.random() * data.response.artists.length)];
-        song = artist.songs[Math.floor(Math.random() * artist.songs.length)]
         console.log("artist object", artist);
-        console.log("song object", song);
-        $("#results").append("Randomly selected artist: "+artist.name+"<br/>" +
-                             "Randomly selected song: " + song.title + "<br/>" +
-                             "-------------------------------------------------- <br/>");
+        artist.spotifyID = spotifyArtistId(artist);
+        playlist.push(artist);
+        getArtistTopTracks(artist);
 
-        spotifyID = spotifyArtistId(artist);
-        playlist.push({artist: artist.name, spotifyID: spotifyID});
-        getArtistTopTracks(playlist[0]);
     });
   };
 
+  // MAKES SPOTIFY API CALL BASED ON THE ARTIST ID AND SETS PROPERTY topTracks AND A randomTrack
   function getArtistTopTracks(artist) {
-    var spotifyID = artist.spotifyID;
-    var topTracksUrl = "https://api.spotify.com/v1/artists/" + spotifyID + "/top-tracks?country=GB";
+    var topTracksUrl = "https://api.spotify.com/v1/artists/" + artist.spotifyID + "/top-tracks?country=" + countryCode;
 
     $.get(topTracksUrl, function(response) {
       artist.topTracks = response.tracks;
-      artist.randomTrack = selectArtistRandomTrack(artist.topTracks);
-      console.log( "preview URL:", artist.randomTrack.preview_url);
+
+      // SELECTS A RANDOM TRACK FROM topTracks
+      artist.randomTrack = function() {
+          return this.topTracks[Math.floor(Math.random() * this.topTracks.length)];
+        }
+
+        alert('Everything has been completed');
     });
   }
 
-  function selectArtistRandomTrack(topTracks) {
-    return topTracks[Math.floor(Math.random() * topTracks.length)];
-  }
-
+  // PICKS OUT THE ARTIST ID FROM THE URI
   function spotifyArtistId(artist) {
     var foreignId = artist.foreign_ids[0].foreign_id
-
     return foreignId.slice(15);
   }
 
