9fe39581c12147cfb43b1416aa6ab64f2c1390a2
Tom Barnes <ruralmyth@gmail.com>
2015-12-18 11:19:04 +0000

    decoupling methods with promises

--- app/js/jPlayer.js
+++ app/js/jPlayer.js
@@ -1,40 +1,24 @@
-// $("#jquery_jplayer_1").jPlayer({
-
-//     play: function(e) {
-//       console.log(e.jPlayer.status.media.title)
-//     },
-//     ready: function () {
-//       console.log('player is ready');
-//       $(this).jPlayer("setMedia", {
-//         title: "THIS IS THE FIRST SONG",
-//         mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3"
-//       });
-//     },
-//     swfPath: "/js",
-//     supplied: "mp3"
-// });
-
-
 var myPlaylist = new jPlayerPlaylist({
   jPlayer: "#jquery_jplayer_1",
   cssSelectorAncestor: "#jp_container_1"
 },
-[     {
-      title:"Lentement",
-      artist:"Miaow",
-      mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-      oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg",
-      poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
-    }], //EMPTY PLAYLIST
+[], //EMPTY PLAYLIST
 {
   playlistOptions: {
     enableRemoveControls: true
   },
   swfPath: "/js",
-  supplied: "ogv, m4v, oga, mp3",
+  supplied: "mp3",
   smoothPlayBar: true,
   keyEnabled: true,
-  ready: console.log('playlist loaded'),
+
+  // ready: console.log('playlist loaded'),
+  
   autoplay: true,
   audioFullScreen: true // Allows the audio poster to go full screen via keyboard
+});
+
+$("#skip").click(function() {
+  console.log("skip button");
+  myPlaylist.next();
 });
--- script.js
+++ script.js
@@ -1,95 +1,122 @@
 $(document).ready(function() {
 
-  var cityName;
-  var country;
-  var countryCode;
-  var playlist = [];
-  var trackUrl;
+  var positionData;
+  var playing = false;
 
-  $('#load-track').click(function() {
-    $.when(getLocation()).done( function() { console.log(playlist) } );
-  });
+  // $('#load-track').click(function() {
+  //   getLocation();
+  // });
 
-  function getLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition, function() { console.log("failed to get position") }, {timeout: 30000});
-  };
-
-  //
-  function showPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var geolocUrl = 'https://maps.googleapis.com/maps/api/geocode/json?&language=en&latlng=' + latitude + "," + longitude
-                     '&key=AIzaSyAGWnWE0GeEPpCYmiy2mXZ9RnDGf_n3JQA';
-
-    $.get(geolocUrl, function(response) {
-
-      var results = response.results
-      if (results[results.length-1].address_components[0].long_name === 'United Kingdom') {
-        for (var result = 0; result < results.length; result++) {
-          for (var component = 0; component < results[result].address_components.length; component++) {
-            if(results[result].address_components[component].types.includes('postal_town')) {
-              cityName = convToParam(results[result].address_components[component].long_name);
-            };
-            if (results[result].address_components[component].types.includes('administrative_area_level_1')) {
-              country = convToParam(results[result].address_components[component].long_name);
-            };
-          };
-        };
-      } else {
-        for (var result = 0; result < results.length; result++) {
-          for (var component = 0; component < results[result].address_components.length; component++) {
-            if(results[result].address_components[component].types.includes('locality')) {
-              cityName = convToParam(results[result].address_components[component].long_name);
-            };
-          };
-        };
-        country = convToParam(results[results.length-1].address_components[0].long_name);
-      };
-
-      countryCode = results[results.length-1].address_components[0].short_name;
-      console.log("cityName = " + cityName, "country = " + country, "countryCode = " + countryCode);
-      console.log(response);
-      getArtists();
+  function getLocation() { // Nb. Error handling has been removed
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        resolve([latitude, longitude]);
+      });
     });
-  };
+  }; 
+
+  function showPosition() { 
+
+    return new Promise(function(resolve, reject) {
+      getLocation().then(function(position) {
+        
+        console.log('THE FIRST PROMISE: ' + position);
+      
+        var latitude = position[0]
+        var longitude = position[1]
+        var geolocUrl = 'https://maps.googleapis.com/maps/api/geocode/json?&language=en&latlng=' + latitude + "," + longitude
+                         '&key=AIzaSyAGWnWE0GeEPpCYmiy2mXZ9RnDGf_n3JQA';
+
+        $.get(geolocUrl, function(response) {
+          var results = response.results
+          if (results[results.length-1].address_components[0].long_name === 'United Kingdom') {
+            for (var result = 0; result < results.length; result++) {
+              for (var component = 0; component < results[result].address_components.length; component++) {
+                if(results[result].address_components[component].types.includes('postal_town')) {
+                  cityName = convToParam(results[result].address_components[component].long_name);
+                };
+                if (results[result].address_components[component].types.includes('administrative_area_level_1')) {
+                  country = convToParam(results[result].address_components[component].long_name);
+                };
+              };
+            };
+          } else {
+            for (var result = 0; result < results.length; result++) {
+              for (var component = 0; component < results[result].address_components.length; component++) {
+                if(results[result].address_components[component].types.includes('locality')) {
+                  cityName = convToParam(results[result].address_components[component].long_name);
+                };
+              };
+            };
+            country = convToParam(results[results.length-1].address_components[0].long_name);
+          };
+          countryCode = results[results.length-1].address_components[0].short_name;
+
+          // console.log("cityName = " + cityName, "country = " + country, "countryCode = " + countryCode);
+          // console.log(response);  
+
+          resolve([cityName, country, countryCode]);
+
+        });
+      }); 
+    });
+  };  
+
 
   // MAKES ECHONEST API CALL BASED ON cityName AND country
-  function getArtists() {
-    var echonestUrl = 'http://developer.echonest.com/api/v4/artist/search?api_key=BG6IJZJJYOKNETBX8' +
-                  '&format=json' +
-                  '&artist_location=' + cityName + '+' + country +
-                  '&min_familiarity=0.1' +
-                  '&sort=familiarity-desc&results=35' +
-                  '&bucket=id:spotify';
+  function getArtists(positionData) {
+    return new Promise(function(resolve, reject) {
+      var cityName = positionData[0];
+      var country = positionData[1];
+      var echonestUrl = 'http://developer.echonest.com/api/v4/artist/search?api_key=BG6IJZJJYOKNETBX8' +
+                    '&format=json' +
+                    '&artist_location=' + cityName + '+' + country +
+                    '&min_familiarity=0.1' +
+                    '&sort=familiarity-desc&results=35' +
+                    '&bucket=id:spotify';
 
-    $.get(echonestUrl, function(data){
-      data.response.artists.forEach(function(artist) {
-        getArtistTopTracks(artist);
+      $.get(echonestUrl, function(data){
+        resolve(data);
       });
-      console.log(playlist);
     });
   };
 
 
 
   // MAKES SPOTIFY API CALL BASED ON THE ARTIST ID AND SETS PROPERTY topTracks AND A randomTrack
-  function getArtistTopTracks(artist) {
-    var spotifyId = spotifyArtistId(artist);
-    var topTracksUrl = "https://api.spotify.com/v1/artists/" + spotifyId + "/top-tracks?country=" + countryCode;
+  // THIS SHOULD BE REFRACTORED INTO TWO METHODS: 1) getTopTracksForAllArtists 2) addTracksToPlaylist
+  // IF POSSIBLE playIfNotPlaying SHOULD BE CALLED FROM OUTSIDE OF THIS FUNCTION
+  // function getArtistTopTracks(artist) {
+  function getArtistTopTracks(artistsObject, positionData) {
+    return new Promise(function(resolve, reject) {
+    artistsObject.response.artists.forEach(function(artist) {  
+        var spotifyId = spotifyArtistId(artist);
+        var countryCode = positionData[2];   
+        var topTracksUrl = "https://api.spotify.com/v1/artists/" + spotifyId + "/top-tracks?country=" + countryCode;  
 
-    $.get(topTracksUrl, function(response) {
-      response.tracks.forEach(function(song) {
-
-        myPlaylist.add({
-          title: song.name,
-          artist: song.artists[0].name,
-          mp3: song.preview_url
-        });
-
+        $.get(topTracksUrl, function(response){ 
+          response.tracks.forEach(function(song) {
+            myPlaylist.add({
+              title: song.name,
+              artist: song.artists[0].name,
+              mp3: song.preview_url
+            });
+          playIfNotPlaying();  
+          }); 
+        }); 
       });
-
-      myPlaylist.play(0);
+      resolve('No data to return'); 
     });
+  }
+
+  // PLAY THE PLAYLIST IF IT'S NOT ALREADY PLAYING
+  function playIfNotPlaying(){
+    if (!playing) {
+      playing = true;
+      myPlaylist.play(0);
+    }; 
   }
 
   // PICKS OUT THE ARTIST ID FROM THE URI
@@ -106,47 +133,16 @@ $(document).ready(function() {
     return result;
   }
 
-
-/////// ************************** PLAYER **************************
-
-  $("#jquery_jplayer_1").jPlayer({
-
-      play: function(e) {
-        console.log(e.jPlayer.status.media.title)
-      },
-      // ready: function () {
-      // //   console.log('player is ready');
-      // //   $(this).jPlayer("setMedia", {
-      // //     title: "THIS IS THE FIRST SONG",
-      // //     mp3: "http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3"
-      // //   });
-      //   // myPlaylist.play(0);
-      // },
-      swfPath: "/js",
-      supplied: "mp3"
+  // CALLING THE FUNCTIONS IN A CHAIN
+  showPosition().then(function(positionPromise) {
+    positionData = positionPromise;
+    console.log('THE SECOND PROMISE: ' + positionData);
+    getArtists(positionData).then(function(artistsObjectPromise) {
+      console.log('THE THIRD PROMISE: (see object below)');
+      console.log(artistsObjectPromise);
+      getArtistTopTracks(artistsObjectPromise, positionData).then(function(topTracksPromise) {
+        console.log('THE FOURTH PROMISE: ' + topTracksPromise);
+      });  
+    });  
   });
-
-  var myPlaylist = new jPlayerPlaylist({
-    jPlayer: "#jquery_jplayer_1",
-    cssSelectorAncestor: "#jp_container_1"
-  },
-  [], //EMPTY PLAYLIST
-  {
-    playlistOptions: {
-      enableRemoveControls: true
-    },
-    swfPath: "/js",
-    supplied: "ogv, m4v, oga, mp3",
-    smoothPlayBar: true,
-    keyEnabled: true,
-    ready: console.log('playlist loaded'),
-    autoplay: true,
-    audioFullScreen: true // Allows the audio poster to go full screen via keyboard
-  });
-
-  $("#skip").click(function() {
-    console.log("skip button");
-    myPlaylist.next();
-  });
-
 });
