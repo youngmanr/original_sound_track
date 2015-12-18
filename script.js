aa597f6c272ca402cc8edf135196ccdf3e6f03a2
Robert Youngman <rob_youngman@yahoo.com>
2015-12-18 13:16:49 +0000

    migrated songkick api calls to promises

--- script.js
+++ script.js
@@ -134,17 +134,19 @@ $(document).ready(function() {
     return result;
   }
 
-  function getEvent(positionData) {
-    var eventUrl = 'https://api.songkick.com/api/3.0/search/locations.json?location=geo:'+
-      positionData.latitude + ',' + positionData.longitude +
-      '&apikey=qMMmyACVKOgL3Kgb' + '&jsoncallback=?';
-    $.getJSON(eventUrl, function(data){
-        var metroAreaID = data.resultsPage.results.location[0].metroArea.id;
-        upcomingEvents(metroAreaID);
+  function getSongKickMetroID(positionData) {
+    return new Promise(function(resolve, reject) {
+      var eventUrl = 'https://api.songkick.com/api/3.0/search/locations.json?location=geo:'+
+        positionData.latitude + ',' + positionData.longitude +
+        '&apikey=qMMmyACVKOgL3Kgb' + '&jsoncallback=?';
+      $.getJSON(eventUrl, function(data){
+          var metroAreaID = data.resultsPage.results.location[0].metroArea.id;
+          resolve(metroAreaID);
+      });
     });
   };
 
-  function upcomingEvents(metroAreaID) {
+  function getUpcomingEvents(metroAreaID) {
     var eventUrl = 'https://api.songkick.com/api/3.0/metro_areas/' +
     metroAreaID +
     '/calendar.json?apikey=qMMmyACVKOgL3Kgb' + '&jsoncallback=?';
@@ -171,6 +173,8 @@ $(document).ready(function() {
         console.log('THE FOURTH PROMISE: ' + topTracksPromise);
       });
     });
-    getEvent(positionData);
+    getSongKickMetroID(positionData).then(function(metroAreaIDPromise) {
+      getUpcomingEvents(metroAreaIDPromise);
+    });
   });
 });
