d2b5ce3b84d5295b135e38fd9e1340540a1bf742
Robert Youngman <rob_youngman@yahoo.com>
2016-01-04 18:24:13 +0000

    wired up familiarity buttons

--- script.js
+++ script.js
@@ -7,6 +7,30 @@ $(document).ready(function() {
   //   getLocation();
   // });
 
+  $("#familiarityLow").click(function() {
+    updateFamiliarity("0.1");
+  });
+
+  $("#familiarityMedium").click(function() {
+    updateFamiliarity("0.5");
+  });
+
+  $("#familiarityHigh").click(function() {
+    updateFamiliarity("0.9");
+  });
+
+  function updateFamiliarity(f) {
+    myPlaylist.remove();
+    getArtists(positionData, f).then(function(artistsObjectPromise) {
+      console.log('Updated Familiarity Artists: (see object below)');
+      console.log(artistsObjectPromise);
+      getArtistTopTracks(artistsObjectPromise, positionData).then(function(topTracksPromise) {
+        console.log("Update MyPlaylist (see below)");
+        console.log(myPlaylist);
+      });
+    });
+  };
+
   function getLocation() { // Nb. Error handling has been removed
     return new Promise(function(resolve, reject) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -106,8 +130,6 @@ $(document).ready(function() {
 
         $.get(topTracksUrl, function(response){
           response.tracks.forEach(function(song) {
-            console.log("song (see below)");
-            console.log(song);
             myPlaylist.add({
               title: song.name,
               artist: song.artists[0].name,
@@ -184,6 +206,8 @@ $(document).ready(function() {
       console.log(artistsObjectPromise);
       getArtistTopTracks(artistsObjectPromise, positionData).then(function(topTracksPromise) {
         console.log('THE FOURTH PROMISE: ' + topTracksPromise);
+        console.log("MyPlaylist (see below)");
+        console.log(myPlaylist);
       });
     });
     getSongKickMetroID(positionData).then(function(metroAreaIDPromise) {
