c6325ce597f39f3de1a44c082737c51534e12404
Robert Youngman <rob_youngman@yahoo.com>
2016-01-06 21:32:37 +0000

    third attempt to fix bug with artist bio popup

--- app/js/jPlayer.js
+++ app/js/jPlayer.js
@@ -63,8 +63,8 @@ function displayCurrentArtist(document, artist, title, poster, bio, news) {
   //$("#playingArtistBiography").append("<a class=\"list-group-item\" href=\"#\""+
   //        '<a data-toggle="modal" data-target="#myModal">' + bio.text.substring(0,500) +'... see more</a>');
 
-  $("#playingArtistBiography").append("<a class=\"list-group-item\" href=\"#myModal\""+
-          '<a data-toggle="modal" data-target="#myModal">' + bio.text.substring(0,500) +'... see more</a>');
+  $("#playingArtistBiography").append("<a class=\"list-group-item\" href=\"#\""+
+          'data-toggle="modal" data-target="#myModal">' + bio.text.substring(0,500) +'... see more</a>');
 
   // Clears previous articles before adding new
   $('#playingArtistNews').html("");
